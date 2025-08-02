d6b06a719e165968d12e6dee80322ee314a62fa6
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-08-03 00:42:09 +0200

    chore: enforce strict types and add Pint configuration

--- config/windy-api.php
+++ config/windy-api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 // config for Benoit VRIGNAUD/WindyApi
 return [
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace WindyApi\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
