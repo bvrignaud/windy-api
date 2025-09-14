c35237b90d7f32ac03aedf668c911348a9f39fb1
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2025-09-14 15:25:59 +0200

    test: add architecture test for enum classes

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch('enums')
+    ->expect('WindyApi\Enums')
+    ->toBeEnums();
